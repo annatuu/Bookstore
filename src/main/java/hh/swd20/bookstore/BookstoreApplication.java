e30cc32df1d5d7f147f87d21611322a90a63e228
annatuu <anna.tuulos@myy.haaga-helia.fi>
2019-09-12 11:48:59 +0300

    chapter 3 e 1

--- src/main/java/hh/swd20/bookstore/BookstoreApplication.java
+++ src/main/java/hh/swd20/bookstore/BookstoreApplication.java
@@ -1,13 +1,26 @@
 package hh.swd20.bookstore;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import hh.swd20.bookstore.domain.Book;
+import hh.swd20.bookstore.domain.BookRepository;
 
 @SpringBootApplication
 public class BookstoreApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookstoreApplication.class, args);
+	}
+	
+	@Bean
+	public CommandLineRunner demo(BookRepository repository) {
+		return (args) -> {
+			repository.save(new Book("Java-ohjelmointi", "Mika Vesterholm - Jorma Kyppö", 2003, "951-762-832-3", 49.99));
+			repository.save(new Book("Suomi-englanti-suomi sanakirja", "Hurme - Malin - Pesonen - Syväoja", 2001, "951-0-24662-X", 54.50));
+		};
 	}
 
 }
--- src/main/java/hh/swd20/bookstore/domain/Book.java
+++ src/main/java/hh/swd20/bookstore/domain/Book.java
@@ -1,6 +1,15 @@
 package hh.swd20.bookstore.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Book {
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private long id;
 	private String title;
 	private String author;
 	private int year;
